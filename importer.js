3645275bcebe0036a3795c643f2537f06347dbb9
Thibault Buatois <thibault.buatois@epita.fr>
2025-03-24 14:03:57 +0100

    Fix navbar

--- importer.js
+++ importer.js
@@ -6,7 +6,7 @@ document.getElementById("template-navbar").innerHTML = `
   <!-- Navbar (placed at the bottom of the header image) -->
   <div class="w3-bar w3-light-grey w3-round w3-display-bottommiddle w3-hide-small" style="bottom:-16px">
     <a href="index.html" class="w3-bar-item w3-button">Liste des années</a>
-    <a href="2024.html" class="w3-bar-item w3-button">2024</a>
+    <a href="2025.html" class="w3-bar-item w3-button">2025</a>
   </div>
 </header>
 `;
@@ -16,7 +16,7 @@ document.getElementById("template-navbar-small").innerHTML = `
 <div class="w3-center w3-light-grey w3-padding-16 w3-hide-large w3-hide-medium">
     <div class="w3-bar w3-light-grey">
         <a href="index.html" class="w3-bar-item w3-button">Liste des années</a>
-        <a href="2024.html" class="w3-bar-item w3-button">2024</a>
+        <a href="2025.html" class="w3-bar-item w3-button">2025</a>
     </div>
 </div>
 `
